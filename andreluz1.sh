eebafcadd60690b248c6f2590b595d4b1f11deb3
Ubuntu <boechat.andre@gmail.com>
2018-06-16 11:53:30 -0700

    msg

--- andreluz1.sh
+++ andreluz1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-git add -A
-git commit -m "$1"
+echo "digite sua querida mensagem"
+read $msg
+git add .
+git config --global push.default matching
+echo "aviso > sera dado o push para a pasta que o senhor clonou!"
+git commit -m msg
 git push
