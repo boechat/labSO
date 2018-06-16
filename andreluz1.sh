eb79e66c789b3612fad7876160c86759a9ca944a
Ubuntu <boechat.andre@gmail.com>
2018-06-16 12:02:13 -0700

    Trabalho para a av2

--- andreluz1.sh
+++ andreluz1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "digite sua querida mensagem"
-read $msg
+
 git add .
 git config --global push.default matching
 echo "aviso > sera dado o push para a pasta que o senhor clonou!"
-git commit -m msg
+git commit -m "Trabalho para a av2"
 git push
